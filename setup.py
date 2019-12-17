d81b12d0a6ddedd92f616fb50ff5420c23103442
Niklas Dittmann <n.dittmann@gastromatic.de>
2019-12-17 16:36:07 +0100

    add setuptools

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sqlstate",
+    version="0.0.1",
+    author="Gastromatic",
+    author_email="n.dittmann@gastromatic.de",
+    description="Convenient SQL operations and reflections using sqlalchemy core",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gastromatic/sqlstate",
+    packages=setuptools.find_packages(),
+    package_data={"sqlstate": ["*.yml"]},
+    python_requires=">=3.5",
+    install_requires=[
+        "sqlalchemy>=1.3.0",
+    ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+    ],
+    keywords="sqlalchemy database",
+)
